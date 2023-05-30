4e4b9fced3a910f238b610b877b2e9c1e0db4863
Timur Karimov <tationkar@gmail.com>
2023-05-30 20:26:21 +0300

    second

---
+++ Sem4-task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// 26 Напишите программу,
+//  которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+Console.Clear();
+
+int Method(int number){
+    int count = 0;
+    while (number > 0)
+    {
+        if (number % 10 > 0)
+            count++;
+            number = number /10;
+    }
+    return count;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Для числа {num} количество цифр {Method(num)}");
--- Sem4_task1/Program.cs
+++ Sem4_task1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//24 Напишите программу, которая принимает на вход число A
+// и выдаёт сумму чисел от 1 до A
+// a = 5
+// sum = 1 + 2 + 3 + 4 + 5
+
+
+int GetSum(int n){
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+        sum += 1;
+    return sum;
+}
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма от 1 до {number} равна {GetSum(number)}");
+
+
