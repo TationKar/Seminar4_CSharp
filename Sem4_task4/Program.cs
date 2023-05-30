82649ea1bb5260fe6be26d53885ad2c4f4ce75f8
Timur Karimov <tationkar@gmail.com>
2023-05-30 20:49:18 +0300

    fourth

---
+++ Sem4_task4/Program.cs
@@ -0,0 +1,11 @@
+﻿// 
+
+
+int Method3(int num){
+
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Для числа {num} .... {Method3(num)}");
