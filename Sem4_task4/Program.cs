973aab825e7f9fa67dbe418f08e10f12c910a34f
Timur Karimov <tationkar@gmail.com>
2023-05-30 22:24:45 +0300

    fourth kir

--- Sem4_task4/Program.cs
+++ Sem4_task4/Program.cs
@@ -1,30 +1,29 @@
 ﻿//30 Напишите программу, которая выводит массив из 8 элементов, 
 //заполненный нулями и единицами в случайном порядке.
- //[1,0,1,1,0,1,0,0]
+//[1,0,1,1,0,1,0,0]
 // int a = new Random().Next(2, 7);
 // int b = new Random().Next(2);
 // Console.WriteLine($"a = {a},b = {b}");
 
-int[] ArrayRand(){
-    int[] digit = new int[8];
-for (int i = 0; i < 8; i++)
-{
-    digit[i] = new Random().Next(0, 2); 
-}
+int[] ArrayRand(int sz){
+int[] digit = new int[sz];
+for (int i = 0; i < sz; i++)
+    digit[i] = new Random().Next(2); 
 return digit;
 }
 
 void printArr(int []arr){
-    for (int i = 0; i < 8; i++)
-{
-    Console.Write($"{arr[i]} ");    
-}
+    // for (int i = 0; i < 8; i++)
+    //     Console.Write($"{arr[i]} ");
+    Console.WriteLine($"[{String.Join(", ", arr)}]");    
 }
 
 Console.Clear();
-int[] arrNum = ArrayRand();
+Console.Write("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine());
+int[] arrNum = ArrayRand(size);
 Console.WriteLine();
 printArr(arrNum);
-Console.WriteLine();
+//Console.WriteLine();
 
 //Console.WriteLine($"[{String.Join("; ---", array)}]");
