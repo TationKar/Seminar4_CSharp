5dfa37f96b351cf712ac5ce0d56a935f25d6c752
Timur Karimov <tationkar@gmail.com>
2023-05-30 21:15:26 +0300

    fourth done

--- Sem4_task4/Program.cs
+++ Sem4_task4/Program.cs
@@ -1,11 +1,28 @@
-﻿// 
+﻿//30 Напишите программу, которая выводит массив из 8 элементов, 
+//заполненный нулями и единицами в случайном порядке.
+ //[1,0,1,1,0,1,0,0]
+// int a = new Random().Next(2, 7);
+// int b = new Random().Next(2);
+// Console.WriteLine($"a = {a},b = {b}");
 
+int[] ArrayRand(){
+    int[] digit = new int[8];
+for (int i = 0; i < 8; i++)
+{
+    digit[i] = new Random().Next(0, 2); 
+}
+return digit;
+}
 
-int Method3(int num){
-
+void printArr(int []arr){
+    for (int i = 0; i < 8; i++)
+{
+    Console.Write($"{arr[i]} ");    
+}
 }
 
 Console.Clear();
-Console.Write("Введите число: ");
-int num = int.Parse(Console.ReadLine());
-Console.WriteLine($"Для числа {num} .... {Method3(num)}");
+int[] arrNum = ArrayRand();
+Console.WriteLine();
+printArr(arrNum);
+Console.WriteLine();
